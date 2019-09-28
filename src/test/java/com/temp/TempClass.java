d7b4e4862e2bb636a3f5203aaa605aada97b1fe0
Sudheer <Sudheer.nallam999@gmail.com>
2019-09-28 18:35:16 +0530

    Updated Test class with third method

--- src/test/java/com/temp/TempClass.java
+++ src/test/java/com/temp/TempClass.java
@@ -5,13 +5,18 @@ import org.testng.annotations.Test;
 public class TempClass {
 			
 		@Test
-		public void firstMethod() {
+		public void afirstMethod() {
 			System.out.println("First Method executed");
 		}
 		
 		@Test
-		public void secondMethod() {
+		public void bsecondMethod() {
 			System.out.println("Second Method executed");
+		}
+		
+		@Test
+		public void cfinalMethod() {
+			System.out.println("Finally i ran local java code in Jenkins");
 		}
 
 	}
