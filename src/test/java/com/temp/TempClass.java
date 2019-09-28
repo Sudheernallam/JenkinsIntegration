ad476ffcd8a47b371f67b605418fb29306288046
Sudheer <Sudheer.nallam999@gmail.com>
2019-09-28 18:31:31 +0530

    First commit

---
+++ src/test/java/com/temp/TempClass.java
@@ -0,0 +1,18 @@
+package com.temp;
+
+import org.testng.annotations.Test;
+
+public class TempClass {
+			
+		@Test
+		public void firstMethod() {
+			System.out.println("First Method executed");
+		}
+		
+		@Test
+		public void secondMethod() {
+			System.out.println("Second Method executed");
+		}
+
+	}
+
